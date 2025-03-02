645ded40e85ac43376d3f20cc8cd6b555589a8c0
hugop <hugopessego@gmail.com>
2025-03-02 19:30:35 +0000

    Refactor order module by replacing controller with facade.
    Replaced `OrderController` with a new `OrderFacade` interface and its implementation, `OrderFacadeImpl`, to streamline module responsibilities. Removed controller-related files and adjusted packaging to align with the modular architecture. This refactoring enhances modularity and simplifies system interactions.

---
+++ src/main/java/com/thedevhorse/modularizearchitecture/facade/OrderFacadeImpl.java
@@ -0,0 +1,6 @@
+package com.thedevhorse.modularizearchitecture.facade;
+
+import com.thedevhorse.modularizearchitecture.order.spi.OrderFacade;
+
+public class OrderFacadeImpl implements OrderFacade {
+}
--- src/main/java/com/thedevhorse/modularizearchitecture/order/controller/package-info.java
+++
@@ -1,4 +0,0 @@
-@org.springframework.modulith.ApplicationModule(
-        allowedDependencies = "order :: *"
-)
-package com.thedevhorse.modularizearchitecture.order.controller;
