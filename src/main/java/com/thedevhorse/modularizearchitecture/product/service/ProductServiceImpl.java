8dc29973fbba6498954baee0cdef7359267b4bca
hugop <hugopessego@gmail.com>
2025-03-04 14:02:57 +0000

    Refactor ProductServiceImpl to remove unused dependency.
    Removed the unused import of OrderFacade to clean up the codebase. This improves maintainability and eliminates unnecessary distractions in the class.

--- src/main/java/com/thedevhorse/modularizearchitecture/product/service/ProductServiceImpl.java
+++ src/main/java/com/thedevhorse/modularizearchitecture/product/service/ProductServiceImpl.java
@@ -1,14 +1,11 @@
 package com.thedevhorse.modularizearchitecture.product.service;
 
-import com.thedevhorse.modularizearchitecture.order.spi.OrderFacade;
 import com.thedevhorse.modularizearchitecture.product.domain.Product;
 import com.thedevhorse.modularizearchitecture.product.spi.ProductService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
-
 
     @Override
     public Product getProductById() {
